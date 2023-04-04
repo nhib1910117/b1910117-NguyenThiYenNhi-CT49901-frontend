010eff92189d07666b77c0ff3816fa1b78e5a9e3
Nhib1910117 <nhib1910117@student.ctu.edu.vn>
2023-04-04 17:10:10 +0700

    Tao trang them mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
-        },
+    },
+    {
+        path: "/contacts",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-      target: "http://localhost:3005/",
+      target: "http://localhost:3001/",
       changeOrigin: true,
       },
     }
